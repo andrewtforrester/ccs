03fd239c476ff1f390ffc0261ccd531f239168fe
Andrew Forrester <atf@fastmail.com>
2023-01-20 17:39:58 -0500

    homepage updates

--- vue/src/components/GlobalHeader.vue
+++ vue/src/components/GlobalHeader.vue
@@ -5,7 +5,7 @@
 				<div class="flex items-center">
 					<div :class="{ 'bg-navy': this.lighttop=='Yes', 'bg-white': this.lighttop=='No' }" class="h-[2rem] w-[2rem] rounded-full mr-4">
 					</div>
-					<div :class="{ 'text-navy': lighttop=='Yes', 'text-white': lighttop=='No' }" class="text-xl font-bold font-serif leading-6 italic hidden lg:block">
+					<div :class="{ 'text-white': lighttop=='Yes', 'text-white': lighttop=='No' }" class="text-xl font-bold font-serif leading-6 italic hidden lg:block">
 						Center for Christianity <br> and Scholarship
 					</div>
 				</div>
@@ -15,25 +15,47 @@
 			<menu>
 				<div class="flex items-center justify-bewtween">
 					<div class="ml-4 relative hidden sm:block group">
-						<h1 class="text-lg font-bold text-navy hover:cursor-pointer text-navy">About</h1>
+						<h1 class="text-lg font-bold hover:cursor-pointer text-white">About</h1>
 	
-						<div class="absolute overflow-y-hidden whitespace-nowrap h-0 rounded-[.2rem] transition-all bg-navy px-4 ease-in-out duration-300 transform group-hover:h-auto group-hover:py-4 text-white">
+						<div class="absolute top-10 overflow-y-hidden whitespace-nowrap h-0 rounded-[.2rem] transition-all bg-navy px-4 ease-in-out duration-300 transform group-hover:h-auto group-hover:py-4 text-white">
 							<a href="/about">
 								<h2 class="text-md hover:text-rose">Who We Are</h2>
 							</a>
 							<a href="/our-staff">
-								<h2 class="text-md hover:text-rose">Our Staff</h2>
+								<h2 class="text-md text hover:text-rose">Leadership</h2>
 							</a>
 							<a href="/our-faculty">
-								<h2 class="text-md hover:text-rose">Our Faculty</h2>
+								<h2 class="text-md hover:text-rose">Faculty Affiliates</h2>
 							</a>
 						</div>
 					</div>
 
 					<div class="ml-4 relative hidden sm:block group">
-						<h1 class="text-lg font-bold text-navy hover:cursor-pointer text-navy">Students</h1>
+						<h1 class="text-lg font-bold hover:cursor-pointer text-white">Events</h1>
+
+						<div class="absolute top-10 overflow-y-hidden whitespace-nowrap h-0 rounded-[.2rem] transition-all bg-navy px-4 ease-in-out duration-300 transform group-hover:h-auto group-hover:py-4 text-white">
+							<a href="">
+								<h2 class="text-md hover:text-rose">Courses</h2>
+							</a>
+							<a href="">
+								<h2 class="text-md hover:text-rose">Reading Groups</h2>
+							</a>
+							<a href="">
+								<h2 class="text-md hover:text-rose">Lectures</h2>
+							</a>
+							<a href="">
+								<h2 class="text-md hover:text-rose">Weekly Wednesday Meal</h2>
+							</a>
+							<a href="">
+								<h2 class="text-md hover:text-rose">Conferences</h2>
+							</a>
+						</div>
+					</div>
+
+					<div class="ml-4 relative hidden sm:block group">
+						<h1 class="text-lg font-bold text-white hover:cursor-pointer text-white">Undergrads</h1>
 	
-						<div class="absolute overflow-y-hidden whitespace-nowrap h-0 rounded-[.2rem] transition-all bg-navy px-4 ease-in-out duration-300 transform group-hover:h-auto group-hover:py-4 text-white">
+						<div class="absolute top-10 overflow-y-hidden whitespace-nowrap h-0 rounded-[.2rem] transition-all bg-navy px-4 ease-in-out duration-300 transform group-hover:h-auto group-hover:py-4 text-white">
 							<a href="">
 								<h2 class="text-md hover:text-rose">Incoming Students</h2>
 							</a>
@@ -43,52 +65,64 @@
 							<a href="">
 								<h2 class="text-md hover:text-rose">Fellows Program</h2>
 							</a>
+						</div>
+					</div>
+
+					<div class="ml-4 relative hidden sm:block group">
+						<h1 class="text-lg font-bold text-white hover:cursor-pointer text-white whitespace-nowrap">Grad Students</h1>
+	
+						<div class="absolute top-10 overflow-y-hidden whitespace-nowrap h-0 rounded-[.2rem] transition-all bg-navy px-4 ease-in-out duration-300 transform group-hover:h-auto group-hover:py-4 text-white">
 							<a href="">
-								<h2 class="text-md hover:text-rose">Courses</h2>
+								<h2 class="text-md hover:text-rose">Incoming Students</h2>
 							</a>
 							<a href="">
-								<h2 class="text-md hover:text-rose">Mentorship</h2>
+								<h2 class="text-md hover:text-rose">Certification Pathway</h2>
 							</a>
 							<a href="">
-								<h2 class="text-md hover:text-rose">Grad Students</h2>
+								<h2 class="text-md hover:text-rose">Fellows Program</h2>
 							</a>
 						</div>
 					</div>
 					
 					
-					<div class="hidden sm:block">
-						<div class="ml-4 relative peer">
-							<h1 class="text-lg font-bold text-navy">Faculty</h1>
-						</div>
-					</div>
-					
-					<div class="hidden sm:block">
-						<div class="ml-4 relative peer">
-							<h1 class="text-lg font-bold text-navy">Friends</h1>
-						</div>
-					</div>
-					
-					<div class="hidden sm:block">
-						<a href="">
-							<div class="ml-4 relative peer">
-								<h1 class="text-lg font-bold text-navy">House</h1>
-							</div>
-						</a>
-					</div>
-					
-					<div class="ml-4 hidden sm:block">
-						<button>
-							<a href="/events">
-								<div class="font-bold text-lg bg-navy px-4 py-1 rounded-full border-2 border-navy text-gray transition ease-in-out duration-200 transform hover:bg-transparent hover:text-navy">
-									Events
-								</div>
+					<div class="ml-4 relative hidden sm:block group">
+						<h1 class="text-lg font-bold text-white hover:cursor-pointer text-white">Faculty</h1>
+
+						<div class="absolute top-10 overflow-y-hidden whitespace-nowrap h-0 rounded-[.2rem] transition-all bg-navy px-4 ease-in-out duration-300 transform group-hover:h-auto group-hover:py-4 text-white">
+							<a href="">
+								<h2 class="text-md hover:text-rose">Triangle Roundtable</h2>
 							</a>
-						</button>
+							<a href="">
+								<h2 class="text-md hover:text-rose">Faculty Reading Groups</h2>
+							</a>
+							<a href="">
+								<h2 class="text-md hover:text-rose">Scholarship Workshop</h2>
+							</a>
+						</div>
 					</div>
+
+					<div class="ml-4 relative hidden sm:block group">
+						<h1 class="text-lg font-bold text-white hover:cursor-pointer text-white">Friends</h1>
+
+						<div class="absolute top-10 overflow-y-hidden whitespace-nowrap h-0 rounded-[.2rem] transition-all bg-navy px-4 ease-in-out duration-300 transform group-hover:h-auto group-hover:py-4 text-white">
+							<a href="">
+								<h2 class="text-md hover:text-rose">Continuing Eduation</h2>
+							</a>
+							<a href="">
+								<h2 class="text-md hover:text-rose">Lend a Hand</h2>
+							</a>
+							<a href="">
+								<h2 class="text-md hover:text-rose">Donate</h2>
+							</a>
+						</div>
+					</div>
+
+					
+
 					<div class="ml-4 hidden md:block">
 						<button>
 							<a href="/give">
-								<div class="font-bold text-lg bg-navy px-4 py-1 rounded-full border-2 border-navy text-gray transition ease-in-out duration-200 transform hover:bg-transparent hover:text-navy">
+								<div class="font-bold text-lg bg-navy px-4 py-1 rounded-full border-2 border-navy text-gray transition ease-in-out duration-200 transform hover:bg-transparent hover:text-white">
 									Give
 								</div>
 							</a>
