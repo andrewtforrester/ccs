b6a54bd33c68b7e894a4381def27c759a99e3ce7
Andrew Forrester <atf@fastmail.com>
2023-04-11 12:57:09 -0400

    attempt bug fix

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-    publicPath: '/vuefiles/static/src/vue/dist/',
-    outputDir: path.resolve(__dirname, '../vuefiles/static/src/vue/dist/'),
+    publicPath: '/static/src/vue/dist/',
+    outputDir: path.resolve(__dirname, '../static/src/vue/dist/'),
     filenameHashing: false, 
     runtimeCompiler: true, 
     devServer: {
