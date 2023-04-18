c548df8bfc7f09bc38d298957b947164bb97456b
Andrew Forrester <atf@fastmail.com>
2023-04-18 11:51:38 -0400

    fix stripe bug

--- static/checkout.js
+++ static/checkout.js
@@ -140,7 +140,7 @@ async function handleSubmit(e) {
     const { error } = await stripe.confirmPayment({
         elements,
         confirmParams: {
-        return_url: "http://localhost:8000/give/success",
+        return_url: "https://ccsduke.org/give/success",
         receipt_email: emailAddress,
         },
     });
