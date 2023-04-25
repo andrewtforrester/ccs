b928887b9cd04c5cf2b1bfb3a1f78f0167f55eb1
Andrew Forrester <atf@fastmail.com>
2023-04-24 20:29:16 -0400

    bug fix

--- home/templatetags/replace.py
+++ home/templatetags/replace.py
@@ -9,31 +9,3 @@ def replace(value,arg):
     arg2 = arg.split(";")[1]
     return value.replace(arg1,arg2)
 
-@register.filter(name='json_ready')
-def json_ready(nav_streamfield):
-    # return json.dumps(nav_streamfield)
-
-    result = []
-
-    for item in nav_streamfield:
-        if item.block_type == "category":
-            result = result + [{
-                "name": item.value["category_name"],
-            }]
-        elif item.block_type == "internal_button":
-            pass
-        elif item.block_type == 'external_button':
-            pass
-
-    return json.dumps(result)
-
-    # result = []
-    # for item in nav_streamfield:
-    #     result = result + [
-    #         {
-    #             "name": json.dumps(item.value["name"]),
-    #             "description": json.dumps(item.value["description"].source),
-    #         }
-    #     ]
-    # return result
-
